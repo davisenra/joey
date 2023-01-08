5b1fa907003cfd31610bb9af5603666974a987d4
Davi Martins <davi.senra98@gmail.com>
2023-01-07 21:05:35 -0300

    API resources

---
+++ backend/app/Http/Resources/TicketResource.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class TicketResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
+     */
+    public function toArray($request)
+    {
+        return [
+            'uuid' => $this->uuid,
+            'user' => $this->user_id,
+            'assigned_agent' => $this->assigned_agent,
+            'title' => $this->title,
+            'description' => $this->description,
+            'categories' => CategoryResource::collection($this->whenLoaded('categories')),
+            'labels' => LabelResource::collection($this->whenLoaded('labels')),
+            'created_at' => $this->created_at,
+        ];
+    }
+}
---
+++ backend/app/Http/Resources/UserResource.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class UserResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
+     */
+    public function toArray($request)
+    {
+        return [
+            'uuid' => $this->uuid,
+            'name' => $this->name.' '.$this->surname,
+            'email' => $this->email,
+            'role' => $this->role,
+            'tickets' => TicketResource::collection($this->whenLoaded('tickets')),
+            'registered_since' => $this->created_at,
+        ];
+    }
+}
