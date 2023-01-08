b0b251526757a31ecb909360d31b1b048df68a6e
Davi Martins <davi.senra98@gmail.com>
2023-01-07 21:04:46 -0300

    API routes

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,7 +1,36 @@
 <?php
 
+use App\Http\Controllers\Category\CategoryController;
+use App\Http\Controllers\Label\LabelController;
+use App\Http\Controllers\Message\MessageController;
+use App\Http\Controllers\Ticket\TicketController;
+use App\Http\Controllers\User\UserController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-   return 'Hello';
+Route::middleware('auth:sanctum')->group(function () {
+    Route::controller(TicketController::class)->group(function () {
+        Route::get('/tickets', 'index');
+        Route::get('/tickets/{ticket:uuid}', 'show');
+        Route::post('/tickets', 'store');
+        Route::put('/tickets/{ticket:uuid}', 'update');
+        Route::patch('/tickets/{ticket:uuid}/assign', 'assignAgent');
+        Route::patch('/tickets/{ticket:uuid}/lock', 'lockTicket');
+        Route::delete('/tickets/{ticket:uuid}', 'destroy');
+    });
+
+    Route::controller(UserController::class)->group(function () {
+        Route::get('/users', 'index');
+        Route::get('/users/{user:uuid}/{tickets?}', 'show');
+    });
+
+    Route::controller(MessageController::class)->group(function () {
+        Route::get('/messages/{ticket:uuid}', 'index');
+        Route::post('/messages/{ticket:uuid}', 'store');
+        Route::delete('/messages/{message}', 'destroy');
+    });
+
+    Route::apiResources([
+        'categories' => CategoryController::class,
+        'labels' => LabelController::class,
+    ]);
 });
